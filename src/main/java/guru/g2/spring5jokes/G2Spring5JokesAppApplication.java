5dc6682bb606d875834c53415514b39b43e15f09
G2HUB <djilali.eljilali@gmail.com>
2021-02-18 20:09:39 +0100

    Create Spring Boot Project closes #1

---
+++ src/main/java/guru/g2/spring5jokes/G2Spring5JokesAppApplication.java
@@ -0,0 +1,13 @@
+package guru.g2.spring5jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2Spring5JokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(G2Spring5JokesAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/g2/spring5jokes/G2Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.g2.spring5jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2Spring5JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
